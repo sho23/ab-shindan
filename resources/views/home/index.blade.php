d1bd549acba196fb27edc82a28758dd7aaa5b93a
sho23 <testuser.shota@gmail.com>
2018-04-18 17:52:48 +0900

    TOPの診断詳細の文字を丸めた

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -5,14 +5,14 @@
     <div class="container">
         <div class="page-header">
             <p class="text-left"></p>
-            <h3 class="text-center">AB診断の一覧！</h3>
+            <h3 class="text-center">AB診断の一覧</h3>
         </div>
         <div class="row astrolist">
             @foreach ($posts as $post)
                 <div class="card col-md-4 col-sm-6 col-xs-12">
 					<div class="card-body text-center">
                         <img src="{{asset('image/image.php?pt=' . $post->title)}}" alt="" class="img-fluid mb-2">
-						<p class="card-text">{{ $post->detail }}</p>
+						<p class="card-text">{{ mb_strimwidth($post->detail, 0, 100, "...") }}</p>
 						<a href="{{ url('/posts', ['post_id' => $post->id]) }}" class="btn btn-primary">この診断で遊ぶ！</a>
 					</div>
 				</div>
